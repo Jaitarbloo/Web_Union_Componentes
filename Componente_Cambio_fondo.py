d88123f634a0a5c3c2954c0e73e301ba221e24c4
Jaitarbloo <jaitarbloo@yahoo.es>
2025-05-17 12:44:15 +0200

    Ordenado código para una mejor visualización

--- Componente_Cambio_fondo.py
+++ Componente_Cambio_fondo.py
@@ -1,16 +1,14 @@
 import reflex as rx
 
-app = rx.App(
-    stylesheets=["/animation.css"],
-)
+app = rx.App (stylesheets=["/animation.css"])  # Tus hojas de estilo si las tienes)
 
 def Cambio_fondo():
+    
     return rx.box(
-        rx.text("Freelance Developer", class_name="hero-text"),
-        class_name="hero-bg",
-        margin_top="20em",
         
-        
-    )
+                rx.text("Freelance Developer", class_name="hero-text"),
+                        class_name="hero-bg",
+                        margin_top="20em",
+                 )
 
 app.add_page(Cambio_fondo, title="Cambio de Fondo")
