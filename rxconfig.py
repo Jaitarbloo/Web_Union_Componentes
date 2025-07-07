c90ced8f792738937332b94841fc371085e7f995
Jaitarbloo <64631993+Jaitarbloo@users.noreply.github.com>
2025-07-07 18:12:16 +0200

    Add files via upload

--- rxconfig.py
+++ rxconfig.py
@@ -1,3 +1,8 @@
 import reflex as rx
 
-config = rx.Config( app_name="Web_Union_componentes")
+config = rx.Config( 
+    
+                app_name="Web_Union_componentes",
+                cors_allouwed_origins=["https://api.zabalgana", "https://localhost:3000"]
+                                       
+                    )
