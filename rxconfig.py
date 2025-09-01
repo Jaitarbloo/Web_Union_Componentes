9cba4c4ea2358f15aed27bcec179a2e37e4374c4
Jaitarbloo <jaitarbloo@yahoo.es>
2025-09-01 17:59:47 +0200

    AÃ±adiendo mejoras y componentes

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,7 @@ import reflex as rx
 config = rx.Config( 
     
                 app_name="Web_Union_componentes",
-                cors_allowed_origins=["https://web-union-componentes.onrender.com", "https://localhost:3000"],
+                cors_allowed_origins=["https://web-union-componentes.onrender.com", "https://localhost:3000", "https://api.zabalgana.com/"],
                 disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"]
                                        
                     )
